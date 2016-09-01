673856ec7df4df59937a6949298ea62b7ef742e9
Sara Grandfield <sara.grandfield@gmail.com>
2016-09-01 18:30:29 -0400

    initial commit, test with hello world

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,11 +1,13 @@
 // Test.cpp : Defines the entry point for the console application.
-//
+// Sara Grandfield 9/1/2016
 
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    return 0;
+	cout << "Hello World" << endl;
+	return 0;
 }
 
